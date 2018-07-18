d7795e427b24b6a99e256868aebd6ebc1d96d700
Kalli <kallirant@gmail.com>
2018-07-18 15:31:16 +0300

    added changed file:new optimizations

--- env_spec.py
+++ env_spec.py
@@ -1,57 +1,22 @@
 import re
 
-def check_latin_chars(input_str):
+def check_input(field_array):
     """
-    function that checks if input_str consists of latin chars 
-    (input_str can contain underscore and numeric digits)
-    """
-    try:
-        input_str.encode('utf-8').decode('ascii')
-    except Exception:
-        return False
-    else:
-        return True
-
-
-def check_symbols(input_str):
-    """
-    checks input_str (must be digit/underscore/uppercase latin char)
-    return False for error about specifications or True if correct
+    Checks syntax of input(uppercase latin chars,numeric digits, underscores).
+    Returns False for error or True for correct syntax.
     """
     alphanumeric_that_does_not_start_with_digit = r'^[A-Z_][0-9A-Z_]*$'
 
-    ret = re.match(alphanumeric_that_does_not_start_with_digit, input_str)
-
-    if ret == None:
-        return False
-    else:
-        return True
-
-
-def check_input(field_array):
-    """
-    Checks syntax of input(uppercase latin chrars,numeric digits, underscores).
-    Returns False for error or True for correct syntax.
-    """
     for line in field_array:       
-        
-        latin_chars = check_latin_chars(line)
-        if(latin_chars == -1):
-            print('1.SYNTAX ERROR: NOT LATIN CHARS!')
+        ret = re.match(alphanumeric_that_does_not_start_with_digit, line)
+
+        if ret is None:
+            print('SYNTAX ERROR:WRONG SYMBOLS!')
             return False
-        
-        #syntax check for existing digits / underscore on string
-        ret = check_symbols(line)
-        if (ret == False):
-            print('2.SYNTAX ERROR:WRONG SYMBOLS!')
-            return False
-    
-    print('---Correct syntax of given specificarions---')
+
     return True
 
-
 def render_env_var_spec(env_var_name):
-    ret_str = ''
     env_var_name_lower = env_var_name.lower()
 
     ret_str = f'<label for="env_spec_{env_var_name_lower}"> {env_var_name}</label>'
@@ -61,27 +26,21 @@ def render_env_var_spec(env_var_name):
     return ret_str
 
 def render_env_spec_to_html(field_array):
-    html_output = []
-    ret = check_input(field_array)  #check syntax
-
-    if (ret == True):
+    if (check_input(field_array)):
+        html_output = []
+       
         for field in field_array:  
             html_output.append(render_env_var_spec(field))  #create html output
 
-        html_output = '\n'.join(html_output)
-   
-    return html_output
-
+        return '\n'.join(html_output)
+    return ''
 
 def main():
-    field_array = []
     field_array = spec_str.split('\n')
-   
     html_output = render_env_spec_to_html(field_array)
 
     return html_output
 
 if __name__ == "__main__":
     spec_str = 'DATABASE_URL\nADMIN_EMAIL\nDEBUG'
-
     print(main())
