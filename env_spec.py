1212adc384dcc19ff15a87281b24ec2b562d2b28
Kalli <kallirant@gmail.com>
2018-07-20 14:23:34 +0300

    changed_file: added types

--- env_spec.py
+++ env_spec.py
@@ -1,41 +1,65 @@
 import re
 
+valid_types_array = [
+    "color",
+    "date",
+    "datetime-local",
+    "email",
+    "month",
+    "number",
+    "password",
+    "tel",
+    "text",
+    "time",
+    "url",
+    "week",
+]
 
-def check_input(field_array):
+
+def check_input(field_type_array):
     """
     Checks syntax of input(uppercase latin chars,numeric digits, underscores).
     Returns False for error or True for correct syntax.
     """
     alphanumeric_that_does_not_start_with_digit = r"^[A-Z_][0-9A-Z_]*$"
 
-    for line in field_array:
-        ret = re.match(alphanumeric_that_does_not_start_with_digit, line)
+    for line in field_type_array:
+        ret = re.match(alphanumeric_that_does_not_start_with_digit, line[0])
 
         if ret is None:
-            # print("SYNTAX ERROR:WRONG SYMBOLS!")
             return False
 
     return True
 
 
-def render_env_var_spec(env_var_name):
+def check_type(input_array):
+    """
+    checks if given type is valid and returns True, else returs False
+    """
+    for type in input_array:
+        if type[1] not in valid_types_array:
+            return False
+    return True
+
+
+def render_env_var_spec(env_var):
+    env_var_name = env_var[0]
+    env_var_type = env_var[1]
     env_var_name_lower = env_var_name.lower()
 
     ret_str = f'<label for="env_spec_{env_var_name_lower}"> {env_var_name}</label>'
     ret_str += "\n"
-    ret_str += (
-        f'<input id="env_spec_{env_var_name_lower}" name="{env_var_name_lower}" />'
-    )
+    ret_str += f'<input id="env_spec_{env_var_name_lower}" name="{env_var_name_lower}" type="{env_var_type}" />'
 
     return ret_str
 
 
-def render_env_spec_to_html(field_array):
-    if check_input(field_array):
+def render_env_spec_to_html(field_type_array):
+    if check_type(field_type_array) and (check_input(field_type_array)):
         html_output = []
 
-        for field in field_array:
-            html_output.append(render_env_var_spec(field))  # create html output
+        for line in field_type_array:
+            html_output.append(render_env_var_spec(line))  # create html output
 
         return "\n".join(html_output)
     return ""
@@ -43,11 +67,16 @@ def render_env_spec_to_html(field_array):
 
 def main():
     field_array = spec_str.split("\n")
-    html_output = render_env_spec_to_html(field_array)
+    field_type_array = []
+
+    for line in field_array:
+        field_type_array.append(line.split(": "))
+
+    html_output = render_env_spec_to_html(field_type_array)
 
     return html_output
 
 
 if __name__ == "__main__":
-    spec_str = "DATABASE_URL\nADMIN_EMAIL\nDEBUG"
+    spec_str = "DATABASE_URL: url\nADMIN_EMAIL: email\nDEBUG: number"
     print(main())
--- env_spec_test.py
+++ env_spec_test.py
@@ -2,17 +2,23 @@ import env_spec
 
 
 def test_invalid_input_non_latin():
-    assert env_spec.render_env_spec_to_html("dαtabase_url") == ""
+    assert env_spec.render_env_spec_to_html(["dαtabase_url"]) == ""
 
 
 def test_invalid_input_start_with_digit():
-    assert env_spec.render_env_spec_to_html("1DATABASE_URL") == ""
+    assert env_spec.render_env_spec_to_html(["1DATABASE_URL, url"]) == ""
+
+
+def test_non_existed_type():
+    assert env_spec.render_env_spec_to_html(["DATABASE_URL, testing"]) == ""
 
 
 def test_valid_input():
-    assert env_spec.render_env_spec_to_html(["DATABASE_URL", "ADMIN_EMAIL"]) == (
+    assert env_spec.render_env_spec_to_html(
+        [["DATABASE_URL", "url"], ["ADMIN_EMAIL", "email"]]
+    ) == (
         '<label for="env_spec_database_url"> DATABASE_URL</label>\n'
-        '<input id="env_spec_database_url" name="database_url" />\n'
+        '<input id="env_spec_database_url" name="database_url" type="url" />\n'
         '<label for="env_spec_admin_email"> ADMIN_EMAIL</label>\n'
-        '<input id="env_spec_admin_email" name="admin_email" />'
+        '<input id="env_spec_admin_email" name="admin_email" type="email" />'
     )
