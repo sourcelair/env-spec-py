f83a228743a16b78050d5d8fe1b5750c2ea6138a
Kalli <kallirant@gmail.com>
2018-07-23 16:31:55 +0300

    changing env_spec:types

--- env_spec.py
+++ env_spec.py
@@ -1,6 +1,6 @@
 import re
 
-valid_types_array = [
+valid_types_list = [
     "color",
     "date",
     "datetime-local",
@@ -16,67 +16,85 @@ valid_types_array = [
 ]
 
 
-def check_input(field_type_array):
+def check_input(input_field):
     """
-    Checks syntax of input(uppercase latin chars,numeric digits, underscores).
+    Checks syntax of input field(uppercase latin chars,numeric digits, underscores).
     Returns False for error or True for correct syntax.
     """
     alphanumeric_that_does_not_start_with_digit = r"^[A-Z_][0-9A-Z_]*$"
 
-    for line in field_type_array:
-        ret = re.match(alphanumeric_that_does_not_start_with_digit, line[0])
+    ret = re.match(alphanumeric_that_does_not_start_with_digit, input_field)
 
-        if ret is None:
-            return False
+    if ret is None:
+        return False
 
     return True
 
 
-def check_type(input_array):
+def check_type(input_type):
     """
-    checks if given type is valid and returns True, else returs False
+    Checks if given type is valid and returns True, else returs False
     """
-    for type in input_array:
-        if type[1] not in valid_types_array:
-            return False
+    if input_type not in valid_types_list:
+        return False
     return True
 
 
 def render_env_var_spec(env_var):
-    env_var_name = env_var[0]
-    env_var_type = env_var[1]
-    env_var_name_lower = env_var_name.lower()
+    """
+    Creates html sting from env_variable (field, type). Returns the string.
+    """
+    try:
+        env_var_field, env_var_type = env_var.split(": ")
+        env_var_field_lower = env_var_field.lower()
 
-    ret_str = f'<label for="env_spec_{env_var_name_lower}"> {env_var_name}</label>'
-    ret_str += "\n"
-    ret_str += f'<input id="env_spec_{env_var_name_lower}" name="{env_var_name_lower}" type="{env_var_type}" />'
+        ret_str = (
+            f'<label for="env_spec_{env_var_field_lower}"> {env_var_field}</label>'
+        )
+        ret_str += "\n"
+        ret_str += f'<input id="env_spec_{env_var_field_lower}" name="{env_var_field_lower}" />'
+        ret_str += "\n"
+        ret_str += f'<input id="env_spec_{env_var_field_lower}" name="{env_var_field_lower}" type="{env_var_type}" />'
+    except:
+        env_var_field = env_var
+        env_var_field_lower = env_var_field.lower()
+
+        ret_str = (
+            f'<label for="env_spec_{env_var_field_lower}"> {env_var_field}</label>'
+        )
 
     return ret_str
 
 
-def render_env_spec_to_html(field_type_array):
-    if check_type(field_type_array) and (check_input(field_type_array)):
-        html_output = []
+def render_env_spec_to_html(input_line):
+    try:
+        input_field, input_type = input_line.split(": ")
 
-        for line in field_type_array:
-            html_output.append(render_env_var_spec(line))  # create html output
+        if check_input(input_field) and check_type(input_type):
+            html_output = render_env_var_spec(input_line)
 
-        return "\n".join(html_output)
+            return html_output
+    except:
+        input_field = input_line
+
+        if check_input(input_field):
+            html_output = render_env_var_spec(input_field)
+
+            return html_output
     return ""
 
 
 def main():
-    field_array = spec_str.split("\n")
-    field_type_array = []
+    field_list = spec_str.split("\n")
+    html_output = ""
 
-    for line in field_array:
-        field_type_array.append(line.split(": "))
-
-    html_output = render_env_spec_to_html(field_type_array)
+    for line in field_list:
+        html_output += render_env_spec_to_html(line)
+        html_output += "\n"
 
     return html_output
 
 
 if __name__ == "__main__":
-    spec_str = "DATABASE_URL: url\nADMIN_EMAIL: email\nDEBUG: number"
+    spec_str = "DATABASE_URL: url\nADMIN_EMAIL: email\nDEBUG: number\nADMIN_NAME"
     print(main())
--- env_spec_test.py
+++ env_spec_test.py
@@ -2,23 +2,20 @@ import env_spec
 
 
 def test_invalid_input_non_latin():
-    assert env_spec.render_env_spec_to_html(["dαtabase_url"]) == ""
+    assert env_spec.render_env_spec_to_html("dαtabase_url") == ""
 
 
 def test_invalid_input_start_with_digit():
-    assert env_spec.render_env_spec_to_html(["1DATABASE_URL, url"]) == ""
+    assert env_spec.render_env_spec_to_html("1DATABASE_URL: url") == ""
 
 
 def test_non_existed_type():
-    assert env_spec.render_env_spec_to_html(["DATABASE_URL, testing"]) == ""
+    assert env_spec.render_env_spec_to_html("DATABASE_URL: testing") == ""
 
 
 def test_valid_input():
-    assert env_spec.render_env_spec_to_html(
-        [["DATABASE_URL", "url"], ["ADMIN_EMAIL", "email"]]
-    ) == (
+    assert env_spec.render_env_spec_to_html("DATABASE_URL: url") == (
         '<label for="env_spec_database_url"> DATABASE_URL</label>\n'
-        '<input id="env_spec_database_url" name="database_url" type="url" />\n'
-        '<label for="env_spec_admin_email"> ADMIN_EMAIL</label>\n'
-        '<input id="env_spec_admin_email" name="admin_email" type="email" />'
+        '<input id="env_spec_database_url" name="database_url" />\n'
+        '<input id="env_spec_database_url" name="database_url" type="url" />'
     )
