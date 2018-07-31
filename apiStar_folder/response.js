df9ce1735db5dfdb83f3288e106302327d16713e
Kalli <kallirant@gmail.com>
2018-07-31 16:13:11 +0300

    new index.html

--- apiStar_folder/response.js
+++
@@ -1,7 +0,0 @@
-const myRequest = new Request('/api/render/', {method: 'GET'});
-
-fetch(myRequest).then(function(response) {
-  response.text().then(function(text) {
-    poemDisplay.textContent = text;
-  });
-});
