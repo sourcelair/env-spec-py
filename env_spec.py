01b9db686c5846cbf2791c5554bf2e8a329d0b6e
Kalli_rnt <kallirant@gmail.com>
2018-08-21 10:29:45 +0000

    new functions: parse, render_to_html

--- env_spec.py
+++ env_spec.py
@@ -1,5 +1,10 @@
 import re
 
+
+class EnvSpecSyntaxError(Exception):
+    pass
+
+
 valid_types_list = [
     "color",
     "date",
@@ -19,138 +24,158 @@ valid_types_list = [
 def create_list(input_str):
     """
     Creates a list from input_str_list.Returns the new list.
+    If input_str=[1,2,], returns empty list(syntax error).
     """
     input_str = input_str.replace("[", "")
     input_str = input_str.replace("]", "")
     new_list = input_str.split(",")
 
+    if "" in new_list:
+        return []
+
     for pos in range(0, len(new_list)):
-        new_list[pos] = str(new_list[pos])
+        new_list[pos] = str(new_list[pos]).strip()
 
     return new_list
 
 
-def check_input(input_field):
-    """
-    Checks syntax of input field(uppercase latin chars,numeric digits, underscores).
-    Returns False for error or True for correct syntax.
-    """
-    alphanumeric_that_does_not_start_with_digit = r"^[A-Z_][0-9A-Z_]*$"
-
-    ret = re.match(alphanumeric_that_does_not_start_with_digit, input_field)
-
-    if ret is None:
-        return False
-
-    return True
-
-
-def check_type(input_type):
-    """
-    Checks if given type is valid and returns True, else returns False.
-    """
-
-    if "[" in input_type and "=" in input_type:
-        env_var_type, value = input_type.split("=")
-        value = value.strip()
-
-        if value in env_var_type:
-            return True
-        else:
-            return False
-    if "=" in input_type:
-        return True
-
-    if "[" in input_type:
-        return True
-    elif input_type not in valid_types_list:
-        return False
-    else:
-        return True
-
-
-def render_env_var_spec(env_var_field, env_var_type):
-    """
-    Creates html string from env_variables (field, type). If env_var_type is a restricted choice,
-    calls create_list to create the list of strs from env_var_type string. Returns the string.
-    """
-
-    env_var_field_lower = env_var_field.lower()
-    env_var_type = env_var_type or "text"
-
-    if "[" not in env_var_type:
-        if "=" in env_var_type:
-            env_var_type, value = env_var_type.split("=")
-            value = value.strip()
-
-            ret_str = (
-                f'<label for="env_spec_{env_var_field_lower}">{env_var_field}</label>\n'
-                f'<input id="env_spec_{env_var_field_lower}" name="{env_var_field_lower}" type="{env_var_type}" value="{value}" />\n'
-            )
-        else:
-            ret_str = (
-                f'<label for="env_spec_{env_var_field_lower}">{env_var_field}</label>\n'
-                f'<input id="env_spec_{env_var_field_lower}" name="{env_var_field_lower}" type="{env_var_type}" />\n'
-            )
-    else:
-        ret_str = ""
-        try:
-            restr_choices, value = env_var_type.split("=")
-            value = value.strip()
-            env_var_type_list = create_list(restr_choices)
-        except:
-            env_var_type_list = create_list(env_var_type)
-
-        ret_str += (
-            f'<label for="env_spec_{env_var_field_lower}">{env_var_field}</label>\n'
-            f'<select id="env_spec_{env_var_field_lower}" name="{env_var_field_lower}">\n'
-        )
-
-        for line in env_var_type_list:
-            if "=" in env_var_type and value == line:
-                ret_str += f'\t<option value="{line}"selected>{line}</option>\n'
-            else:
-                ret_str += f'\t<option value="{line}">{line}</option>\n'
-
-        ret_str += "</select>\n"
-
-    return ret_str
-
-
-def render_env_spec_to_html(input_str):
-    """
-    Takes the whole string and splits it first by \n and then by :, if possible. Returns html output or "".
-    """
+def render_to_html(env_spec_list):
     html_output = ""
 
-    lines = input_str.split("\n")
+    for dict in env_spec_list:
+        if dict["choices"] is None:
+            if dict["default_value"] is None:
+                ret_str = (
+                    f'<label for="env_spec_{dict["name"].lower()}">{dict["name"]}</label>\n'
+                    f'<input id="env_spec_{dict["name"].lower()}" name="{dict["name"].lower()}" type="{dict["type"]}" />\n'
+                )
+            elif dict["default_value"] is not None:
+                ret_str = (
+                    f'<label for="env_spec_{dict["name"].lower()}">{dict["name"]}</label>\n'
+                    f'<input id="env_spec_{dict["name"].lower()}" name="{dict["name"].lower()}" type="{dict["type"]}" value="{dict["default_value"]}" />\n'
+                )
+        elif dict["choices"] is not None:
+            ret_str = (
+                f'<label for="env_spec_{dict["name"].lower()}">{dict["name"]}</label>\n'
+                f'<select id="env_spec_{dict["name"].lower()}" name="{dict["name"].lower()}">\n'
+            )
 
-    for line in lines:
-        try:
-            input_field, input_type = line.split(": ")
-
-            if check_input(input_field) and check_type(input_type):
-                html_output += render_env_var_spec(input_field, input_type)
-            else:
-                return ""
-        except:
-            input_field = line
-
-            if check_input(input_field):
-                html_output += render_env_var_spec(input_field, "")
-            else:
-                return ""
-
+            for x in range(0, len(dict["choices"])):
+                if (
+                    dict["default_value"] is not None
+                    and dict["default_value"] == dict["choices"][x]
+                ):
+                    ret_str += f'\t<option value="{dict["choices"][x]}"selected>{dict["choices"][x]}</option>\n'
+                else:
+                    ret_str += f'\t<option value="{dict["choices"][x]}">{dict["choices"][x]}</option>\n'
+            ret_str += "</select>\n"
+        html_output += ret_str
     return html_output
 
 
+def parse(env_spec_text):
+    env_spec_list = []
+
+    alphanumeric_that_does_not_start_with_digit = r"^[A-Z_][0-9A-Z_]*$"
+    name_regex = r"^(.+)\:(.+)$"
+    choices_regex = r"^(.+)\]"
+    default_values_regex = r"^(.+)\=(.+)$"
+
+    lines = env_spec_text.split("\n")
+
+    for line in lines:
+        line_dict = {}
+
+        name_match = re.match(name_regex, line)
+        default_value_match = re.match(default_values_regex, line)
+
+        if name_match:
+            name = name_match.groups()[0]
+            line = name_match.groups()[1]
+
+            ret = re.match(alphanumeric_that_does_not_start_with_digit, name)
+
+            if ret is None:
+                try:
+                    raise EnvSpecSyntaxError
+                except EnvSpecSyntaxError:
+                    return []
+
+            line_dict["name"] = name
+
+            choices_match = re.match(choices_regex, line)
+
+            if choices_match:
+                choices_str = choices_match.groups()[0]
+
+                choices = create_list(choices_str)
+                if not choices:
+                    try:
+                        raise EnvSpecSyntaxError
+                    except EnvSpecSyntaxError:
+                        return []
+                line_dict["choices"] = choices
+            else:
+                line_dict["choices"] = None
+
+            if default_value_match:
+                default_value = default_value_match.groups()[1]
+                default_value = default_value.strip()
+
+                if choices_match:
+                    if default_value not in choices or default_value == "":
+                        try:
+                            raise EnvSpecSyntaxError
+                        except EnvSpecSyntaxError:
+                            return []
+                line_dict["default_value"] = default_value
+            else:
+                line_dict["default_value"] = None
+
+            if not choices_match:
+                if default_value_match:
+                    type_t = default_value_match.groups()[0]
+                else:
+                    type_t = line
+
+                type_t = type_t.strip()
+
+                if type_t not in valid_types_list:
+                    try:
+                        raise EnvSpecSyntaxError
+                    except EnvSpecSyntaxError:
+                        return []
+                line_dict["type"] = type_t
+            else:
+                type_t = "text"
+                line_dict["type"] = type_t
+        else:
+            name = line.strip()
+            ret = re.match(alphanumeric_that_does_not_start_with_digit, name)
+
+            if ret is None:
+                try:
+                    raise EnvSpecSyntaxError
+                except EnvSpecSyntaxError:
+                    return []
+            type_t = "text"
+            line_dict["name"] = name
+            line_dict["type"] = type_t
+            line_dict["choices"] = None
+            line_dict["default_value"] = None
+
+        env_spec_list.append(line_dict)
+
+    return env_spec_list
+
+
 def main():
-    html_output = render_env_spec_to_html(spec_str)
+    env_spec_list = parse(spec_str)
+    # print(env_spec_list)
+    html_output = render_to_html(env_spec_list)
     return html_output
 
 
 if __name__ == "__main__":
-    spec_str = (
-        "DEBUG: [0,1]= 1\nENVIRONMENT: [production,staging,development]= development"
-    )
+    spec_str = "ADMIN_EMAIL: email\nDEBUG: number\nADMIN_NAME"
     print(main())
--- env_spec_test.py
+++ env_spec_test.py
@@ -2,20 +2,20 @@ import env_spec
 
 
 def test_invalid_input_non_latin():
-    assert env_spec.render_env_spec_to_html("dαtabase_url") == ""
+    assert env_spec.parse("dαtabase_url") == "[]"
 
 
 def test_invalid_input_start_with_digit():
-    assert env_spec.render_env_spec_to_html("1DATABASE_URL: url") == ""
+    assert env_spec.parse("1DATABASE_URL: url") == "[]"
 
 
 def test_non_existed_type():
-    assert env_spec.render_env_spec_to_html("DATABASE_URL: testing") == ""
+    assert env_spec.parse("DATABASE_URL: testing") == "[]"
 
 
 def test_valid_input_types():
-    assert env_spec.render_env_spec_to_html(
-        "ADMIN_EMAIL: email\nDEBUG: number\nADMIN_NAME"
+    assert env_spec.render_to_html(
+        "[{'name': 'ADMIN_EMAIL', 'choices': None, 'default_value': None, 'type': 'email'}, {'name': 'DEBUG', 'choices': None, 'default_value': None, 'type': 'number'}, {'name': 'ADMIN_NAME', 'type': 'text', 'choices': None, 'default_value': None}]"
     ) == (
         '<label for="env_spec_admin_email">ADMIN_EMAIL</label>\n'
         '<input id="env_spec_admin_email" name="admin_email" type="email" />\n'
@@ -23,49 +23,4 @@ def test_valid_input_types():
         '<input id="env_spec_debug" name="debug" type="number" />\n'
         '<label for="env_spec_admin_name">ADMIN_NAME</label>\n'
         '<input id="env_spec_admin_name" name="admin_name" type="text" />\n'
-    )
-
-
-def test_restricted_choices():
-    assert env_spec.render_env_spec_to_html(
-        "DEBUG: [0,1]\nENVIRONMENT: [production,staging,development]"
-    ) == (
-        '<label for="env_spec_debug">DEBUG</label>\n'
-        '<select id="env_spec_debug" name="debug">\n'
-        '\t<option value="0">0</option>\n'
-        '\t<option value="1">1</option>\n'
-        "</select>\n"
-        '<label for="env_spec_environment">ENVIRONMENT</label>\n'
-        '<select id="env_spec_environment" name="environment">\n'
-        '\t<option value="production">production</option>\n'
-        '\t<option value="staging">staging</option>\n'
-        '\t<option value="development">development</option>\n'
-        "</select>\n"
-    )
-
-
-def test_wrong_default_values():
-    assert (
-        env_spec.render_env_spec_to_html(
-            "DEBUG: [0,1]=1\nENVIRONMENT: [production,staging,development]=develoent"
-        )
-        == ""
-    )
-
-
-def test_default_values():
-    assert env_spec.render_env_spec_to_html(
-        "DEBUG: [0,1]= 1\nENVIRONMENT: [production,staging,development]= development"
-    ) == (
-        '<label for="env_spec_debug">DEBUG</label>\n'
-        '<select id="env_spec_debug" name="debug">\n'
-        '\t<option value="0">0</option>\n'
-        '\t<option value="1"selected>1</option>\n'
-        "</select>\n"
-        '<label for="env_spec_environment">ENVIRONMENT</label>\n'
-        '<select id="env_spec_environment" name="environment">\n'
-        '\t<option value="production">production</option>\n'
-        '\t<option value="staging">staging</option>\n'
-        '\t<option value="development"selected>development</option>\n'
-        "</select>\n"
     )
